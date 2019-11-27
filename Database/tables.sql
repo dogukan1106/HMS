5c2607e1308bc118b003d908f24953b9003fabb9
ertugrulozvardar <44060683+ertugrulozvardar@users.noreply.github.com>
2019-11-27 17:39:26 +0300

    Update tables.sql

--- Database/tables.sql
+++ Database/tables.sql
@@ -11,3 +11,14 @@ CREATE TABLE hms.Patient
   UNIQUE (p_Tckn),
   UNIQUE (p_Email)
 );
+
+CREATE TABLE hms.Doctor
+(
+  d_id INT NOT NULL,
+  d_Name VARCHAR(50) NOT NULL,
+  d_Email VARCHAR(40) NOT NULL,
+  d_Hashpw VARCHAR(16) NOT NULL,
+  departmentName VARCHAR(35) NOT NULL,
+  PRIMARY KEY (d_id),
+  UNIQUE (d_Email)
+);
