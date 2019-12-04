d2b9e7b03b38a639c8538ed676b560ebf7e3c5d9
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2019-12-04 23:20:24 +0300

    Update Controller.java

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -30,7 +30,32 @@ public class Controller {
             }
     }
     
+    public void addDoctor(){
+        sql= "INSERT INTO doctor(d_Name, d_Email, d_Hashpw, departmentName) " +
+                "VALUES('Emre Karakuz', 'emrekarakuz@gmail.com', '12345678', 'Cardiology')";
+        //Should be taken as input
+        try {
+            stmt.executeUpdate(sql);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
+    public void addPatient(){
+        sql= "INSERT INTO patient(p_Tckn, p_Name, p_Email, p_Hashpw) " +
+                "VALUES('12345678901','Emre Karakuz','emrekarakuz@gmail.com','12345678')";
+        //Should be taken as input
+        //Hash value of password
+        //TCKN and e-mail should be checked before
+
+        try {
+            stmt.executeUpdate(sql);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
     
 
 
