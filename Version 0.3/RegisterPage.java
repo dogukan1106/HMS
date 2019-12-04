f30b045ec314d20a25b54d9e381930754330fb9c
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2019-12-05 02:58:17 +0300

    Update RegisterPage.java

--- Version 0.3/RegisterPage.java
+++ Version 0.3/RegisterPage.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import javax.swing.*;
 
 public class RegisterPage {
+    Controller cont;
     private JFrame frame;
     private JTextField username;
     private JPasswordField password;
@@ -15,7 +16,8 @@ public class RegisterPage {
     /**
      * Create the application.
      */
-    public RegisterPage() {
+    public RegisterPage(Controller cont) {
+        this.cont = cont;
         initialize();
     }
 
@@ -65,7 +67,7 @@ public class RegisterPage {
 
         JButton register = new JButton("Register");
         //register.addActionListener(new CheckRegisterListener(frame));
-        register.addActionListener(new RegisterInputListener(frame,username,password,tckn));
+        register.addActionListener(new RegisterInputListener(frame,cont,username,password,tckn));
         register.setBounds(199, 228, 87, 23);
         frame.getContentPane().add(register);
         frame.setVisible(true);
