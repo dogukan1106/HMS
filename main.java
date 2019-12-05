ce40016a85c2dc661cfb78754a25d7e8b2382543
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2019-12-05 12:00:21 +0300

    Update main.java

--- main.java
+++ main.java
@@ -2,25 +2,6 @@ import java.sql.*;
 
 public class main {
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement stmt = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/hms", "root", "13212karE");
-            stmt = conn.createStatement();
-            System.out.println("Connected!");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Problem!");
-        }
-
-
-        Controller cont = new Controller(conn,stmt,pstmt);
-        //cont.addAdmin();
-        //cont.addDoctor();
-        //cont.addPatient();
-        
         MainPage mPage = new MainPage(cont);
-
     }
 }
