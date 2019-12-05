18e44b038dcf7f80acbd8d0129327c2ca1caece8
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2019-12-05 03:01:09 +0300

    Update RegisterInputListener.java

--- Version 0.3/RegisterInputListener.java
+++ Version 0.3/RegisterInputListener.java
@@ -7,27 +7,42 @@ import javax.swing.*;
 @SuppressWarnings("deprecation")
 
 public class RegisterInputListener implements ActionListener{
+    Controller cont;
     JFrame frame;
     JTextField name;
     JPasswordField password;
     JTextField tckn;
 
-    public RegisterInputListener(JFrame frame, JTextField name, JPasswordField password, JTextField tckn){
+    public RegisterInputListener(JFrame frame, Controller cont, JTextField name, JPasswordField password, JTextField tckn){
         this.frame = frame;
         this.name = name;
         this.password = password;
         this.tckn = tckn;
+        this.cont = cont;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String nameInput = name.getText();   //receive input from text field
+       String nameInput = name.getText();
+        nameInput = "'" + nameInput + "'";
+
         String tcknInput = tckn.getText();
-        String passwordInput = new PasswordHash(password.getText()).hashed;
+        tcknInput = "'" + tcknInput + "'";
 
+        String p = password.getText();
+        String passwordInput = new PasswordHash(p).hash();
 
-        System.out.println("name: " + nameInput);
-        System.out.println("password: " + passwordInput);
-        System.out.println("tckn: " + tcknInput + "\n");
+        passwordInput = "'" + passwordInput + "'";
+
+        //System.out.println("name: " + nameInput);
+        //System.out.println("password: " + passwordInput);
+        //System.out.println("tckn: " + tcknInput + "\n");
+        
+         try {
+            cont.addPatient(nameInput,passwordInput, "'emrekarakuz@gmail.com'",tcknInput);
+        } catch(Exception exc){
+
+        }
+        
     }
 }
