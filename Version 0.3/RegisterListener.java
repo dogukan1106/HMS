34eb3900436d8ab927de38c04923a34f2446e7f0
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2019-12-05 12:05:26 +0300

    Update RegisterListener.java

--- Version 0.3/RegisterListener.java
+++ Version 0.3/RegisterListener.java
@@ -1,20 +1,15 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
 
 public class RegisterListener implements ActionListener {
     JFrame frame;
-    Controller cont;
-    public RegisterListener(JFrame frame, Controller cont) {
+    public RegisterListener(JFrame frame) {
         this.frame = frame;
-        this.cont = cont;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        RegisterPage rPage = new RegisterPage(cont);
+        RegisterPage rPage = new RegisterPage();
     }
 }
